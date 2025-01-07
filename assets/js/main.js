fb009a7d230b1bdd054427c1e048a83be894546a
JJ6486 <jinashjaleel9@gmail.com>
2025-01-07 13:12:49 +0530

    variables listing

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// variables for navbar menu toggle
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+// variables for navbar search toggle
